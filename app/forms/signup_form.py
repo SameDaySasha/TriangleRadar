1dc52c2f148935cda67daf00e71581165914a0f9
Alex Florea <alexflorea123@gmail.com>
2024-02-12 14:34:39 -0500

    PleaseWork

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,27 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
+from app.models import Player  # Updated import
 
-
-def user_exists(form, field):
-    # Checking if user exists
+def email_exists(form, field):
+    # Checking if email is already in use by a player
     email = field.data
-    user = User.query.filter(User.email == email).first()
-    if user:
+    player = Player.query.filter(Player.email == email).first()
+    if player:
         raise ValidationError('Email address is already in use.')
 
-
 def username_exists(form, field):
-    # Checking if username is already in use
+    # Checking if username is already in use by a player
     username = field.data
-    user = User.query.filter(User.username == username).first()
-    if user:
+    player = Player.query.filter(Player.username == username).first()
+    if player:
         raise ValidationError('Username is already in use.')
-
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField(
+        'email', validators=[DataRequired(), Email(), email_exists])
     password = StringField('password', validators=[DataRequired()])
